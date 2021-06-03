f6edbd8462c1e29fcd9da8f3f504be90188bbc6f
humphreyliu <humphrey.liuxj@gmail.com>
2021-06-03 08:31:29 +0100

    Make the clipped image to be square.

--- image_retrieval_resnet/utils/datasets.py
+++ image_retrieval_resnet/utils/datasets.py
@@ -169,9 +169,11 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         h, w = item['shape']
 
         b = item['label'][1:] * [w, h, w, h]  # box
+        b[2:] = b[2:].max()  # rectangle to square
+        b[2:] = b[2:] * 1.1 + 20  # pad
         b = xywh2xyxy(b.reshape(-1, 4)).ravel().astype(np.int)
 
-        b[[0, 2]] = np.clip(b[[0, 2]], 0, w - 1)  # clip boxes outside of image
-        b[[1, 3]] = np.clip(b[[1, 3]], 0, h - 1)
+        b[[0, 2]] = np.clip(b[[0, 2]], 0, w)  # clip boxes outside of image
+        b[[1, 3]] = np.clip(b[[1, 3]], 0, h)
 
         return img[b[1]:b[3], b[0]:b[2]], b
