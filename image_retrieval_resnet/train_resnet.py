Fix bug of wrong dimension of image.

image_retrieval_resnet/train_resnet.py
@@ -89,10 +89,11 @@ def main():
     transform = transforms.Compose([
             transforms.Resize([args.image_size, args.image_size]),
             transforms.ToTensor(),
-            normalize,
+            # normalize,
         ])
     # Trainloader
     train_loader, dataset = create_dataloader(train_path, args.batch_size, cache=args.cache_image, transform=transform)
+    dataset.classes = names;
 
     val_loader = create_dataloader(test_path, args.batch_size, cache=False, transform=transform)[0]
 
@@ -309,6 +310,24 @@ def show_train_hist(hist, title, show=False, save=False, path='Train_hist.png'):
         plt.show()
     else:
         plt.close()
+
+
+def show_dataset(dataset, channels, dim=(5, 5), path='result.png'):
+    figure = plt.figure(figsize=(8, 8))
+    cols = dim[0]
+    rows = dim[1]
+    for i in range(1, cols * rows + 1):
+        sample_idx = torch.randint(len(dataset), size=(1,)).item()
+        img, label = dataset[sample_idx]
+        figure.add_subplot(rows, cols, i)
+        plt.title(dataset.classes[label])
+        plt.axis("off")
+        if channels == 1:
+            plt.imshow(img.squeeze())
+        else:
+            # rearrange the order
+            plt.imshow(img.squeeze().permute(1,2,0))
+    plt.savefig(path)
 
 if __name__ == '__main__':
     main()

image_retrieval_resnet/utils/datasets.py
@@ -17,7 +17,6 @@ from utils.general import torch_distributed_zero_first, xywh2xyxy
 
 img_formats = ['.bmp', '.jpg', '.jpeg', '.png', '.tif', '.tiff', '.dng']
 
-
 # Get orientation exif tag
 for orientation in ExifTags.TAGS.keys():
     if ExifTags.TAGS[orientation] == 'Orientation':
@@ -31,17 +30,18 @@ def get_hash(files):
 
 def exif_size(img):
     # Returns exif-corrected PIL size
-    s = img.size  # (width, height)
+    s = (img.size[1], img.size[0])  # (0 is width, 1 is height)
     try:
         rotation = dict(img._getexif().items())[orientation]
         if rotation == 6:  # rotation 270
             s = (s[1], s[0])
         elif rotation == 8:  # rotation 90
             s = (s[1], s[0])
-    except:
+    except Exception as e:
         pass
 
     return s
+
 
 def create_dataloader(path, batch_size, cache=True, transform=None):
     # Make sure only the first process in DDP process the dataset first, and the following others can use the cache.
@@ -144,8 +144,13 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         if self.cache_images and self.imgs[index] is not None:
             img = self.imgs[index]
         else:
-            img = self.load_image(index)
-            img = Image.fromarray(img)
+            try:
+                img, b = self.load_image(index)
+                img = Image.fromarray(img)
+            except Exception as e:
+                print(item)
+                print(b)
+                print(e)
 
             if self.transform is not None:
                 img = self.transform(img)
@@ -169,5 +174,4 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         b[[0, 2]] = np.clip(b[[0, 2]], 0, w - 1)  # clip boxes outside of image
         b[[1, 3]] = np.clip(b[[1, 3]], 0, h - 1)
 
-        return img[b[1]:b[3], b[0]:b[2]]
-
+        return img[b[1]:b[3], b[0]:b[2]], b